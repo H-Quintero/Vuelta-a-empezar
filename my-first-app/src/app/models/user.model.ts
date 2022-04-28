0feb09b548887f68a745193156f3572b0aca0ccb
unknown <hugoquinterogarcia@gmail.com>
2022-04-28 11:33:08 +0200

    Mas de componentes y modelos

--- my-first-app/src/app/models/user.model.ts
+++ my-first-app/src/app/models/user.model.ts
@@ -1,5 +1,9 @@
 export interface UserModel {
   name: string;
   age: number;
-  gender?: 'male'| 'famale' | 'non-binary'
+  gender?: 'male' | 'famale' | 'non-binary';
+}
+export interface ImgModel {
+  src: string;
+  alt: string;
 }
--- my-first-app/src/app/student-list/student-list.component.ts
+++ my-first-app/src/app/student-list/student-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImgModel } from '../models/user.model';
 
 @Component({
   selector: 'app-student-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-list.component.scss']
 })
 export class StudentListComponent implements OnInit {
+  public numList: number[] = [1, 2, 3, 4];
+  img: ImgModel = {
+    src: 'https://estaticos.muyinteresante.es/media/cache/400x300_thumb/uploads/images/gallery/5937e90a5bafe882f5bc09e6/gatitos-cesta-redes.jpg',
+    alt: 'Gatitos'
+  }
 
   constructor() { }
 
